7732eba222f4b7f35e0cc87e4ba7065e42e5ff32
Livio AM <68549822+livioangelim@users.noreply.github.com>
2024-09-08 14:34:05 -0400

    Delete foreign directory

--- d387-main/d387-main/src/main/java/edu/wgu/d387_sample_code/repository/ReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.wgu.d387_sample_code.repository;
-
-
-import edu.wgu.d387_sample_code.entity.ReservationEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ReservationRepository extends CrudRepository<ReservationEntity, Long> {
-}
--- d387-main/d387-main/src/main/java/edu/wgu/d387_sample_code/rest/ResourceConstants.java
+++
@@ -1,5 +0,0 @@
-package edu.wgu.d387_sample_code.rest;
-
-public class ResourceConstants {
-    public static final String ROOM_RESERVATION_V1 = "/room/reservation/v1";
-}
