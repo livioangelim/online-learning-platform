48092402224d2de12450bcc644182a96d650cb95
LIVIO ANGELIM MORORO <lange70@wgu.edu>
2024-09-08 10:35:16 -0400

    Added Spring Initializr dependencies for: Web, MySQL and JPA, Security, Lombok, Thymeleaf, Validation and Mail Sender

---
+++ online-learning-platform/src/test/java/com/lvo/online_learning_platform/OnlineLearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvo.online_learning_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
